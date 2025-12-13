b3a7773cc5c1c1427beaf98b2157d194b7bbcea0
IanSeed <shibacachorrogamer@gmail.com>
2025-12-12 21:56:19 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -35,12 +35,6 @@ def cadastro_view(request):
     
     return render(request,'registration/cadastro.html',{'form': form})
 
-def jogo_detalhe_view(request):
-    return render(
-        request,
-        jogo_detalhe.html
-    )
-
 
 @login_required
 def adicionar_carrinho(request, jogo_id):
